ca5a5cf87151b2ffa2178c19980052b112fcfc06
marek <masitko@gmail.com>
2017-06-17 21:25:05 +0100

    fix for response controller

--- Controller/Payment/Response.php
+++ Controller/Payment/Response.php
@@ -12,12 +12,12 @@ class Response extends \Magento\Payl8rPaymentGateway\Controller\Payment {
     $publicKey = $this->config->getValue('merchant_gateway_key');
     $params = $this->request->getParams();
 
-    $this->logger->debug(array('RESPONSE!!!!!'));
-    $this->logger->debug($publicKey);
-    $this->logger->debug($params);
-    
     echo var_export($params);
-    echo var_export($order);
+//    echo var_export($order);
+    
+    $this->logger->debug(array('RESPONSE!!!!!'));
+    $this->logger->debug(array($publicKey));
+    $this->logger->debug($params);
     
     die('test index');
   }
