67c28ae7434b2b64ad68e09d4accf77f1b08f3d0
marek <masitko@gmail.com>
2017-06-18 19:58:29 +0100

    added validation for GB only

--- Gateway/Validator/CountryValidator.php
+++ Gateway/Validator/CountryValidator.php
@@ -6,48 +6,34 @@ use Magento\Framework\App\ObjectManager;
 use Magento\Payment\Gateway\Validator\AbstractValidator;
 use Magento\Payment\Gateway\Validator\ResultInterfaceFactory;
 use Magento\Payment\Gateway\ConfigInterface;
-//use Magento\Payment\Model\Method\Logger;
 use Psr\Log\LoggerInterface;
 
+class CountryValidator extends AbstractValidator {
 
-class CountryValidator extends AbstractValidator
-{
-  
-    private $logger;
-    private $config;
-    
-    public function __construct(
-        ResultInterfaceFactory $resultFactory,
-        ConfigInterface $config,
-        LoggerInterface $logger = null
-    ) {
-        $this->config = $config;
-        $this->logger = $logger ?: ObjectManager::getInstance()->get(LoggerInterface::class);
-        $this->logger->info('Country Validator Constructor!!!');
-        parent::__construct($resultFactory);
+  private $logger;
+  private $config;
+
+  public function __construct(
+  ResultInterfaceFactory $resultFactory, ConfigInterface $config, LoggerInterface $logger = null
+  ) {
+    $this->config = $config;
+    $this->logger = $logger ?: ObjectManager::getInstance()->get(LoggerInterface::class);
+    $this->logger->info('Country Validator Constructor!!!');
+    parent::__construct($resultFactory);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function validate(array $validationSubject) {
+    $isValid = true;
+
+    $this->logger->info('Country Validator!!!');
+    if ($validationSubject['country'] !== 'GB') {
+      $isValid = false;
     }
-  
-    /**
-     * @inheritdoc
-     */
-    public function validate(array $validationSubject)
-    {
-        $isValid = true;
-        $storeId = $validationSubject['storeId'];
 
-        $this->logger->info('Country Validator!!!');
-        
-        if ((int)$this->config->getValue('allowspecific', $storeId) === 1) {
-            $availableCountries = explode(
-                ',',
-                $this->config->getValue('specificcountry', $storeId)
-            );
+    return $this->createResult($isValid);
+  }
 
-            if (!in_array($validationSubject['country'], $availableCountries)) {
-                $isValid =  false;
-            }
-        }
-
-        return $this->createResult($isValid);
-    }
 }
